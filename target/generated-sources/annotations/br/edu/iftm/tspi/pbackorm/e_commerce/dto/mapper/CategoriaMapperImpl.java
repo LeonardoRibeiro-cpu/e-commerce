11ce97934ee482485431bb41b670c72d958a1aba
LeonardoRibeiro-cpu <leonardo.ribeiro@estudante.iftm.edu.br>
2026-09-04 15:48:55 -0300

    e-comeerce

--- target/generated-sources/annotations/br/edu/iftm/tspi/pbackorm/e_commerce/dto/mapper/CategoriaMapperImpl.java
+++ target/generated-sources/annotations/br/edu/iftm/tspi/pbackorm/e_commerce/dto/mapper/CategoriaMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-21T13:43:58-0300",
-    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.46.0.v20260407-0427, environment: Java 21.0.10 (Eclipse Adoptium)"
+    date = "2026-09-04T15:43:12-0300",
+    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
 )
 @Component
 public class CategoriaMapperImpl implements CategoriaMapper {
